cbdfd37a2b01fff5e58dd90cb31f6a12bf1b0abb
harpreetathwal <ha227@cornell.edu>
2025-05-27 17:54:45 -0700

    Initial Commit

---
+++ EaglePuffs/EaglePuffsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaglePuffsApp.swift
+//  EaglePuffs
+//
+//  Created by Harpreet Athwal on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct EaglePuffsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
